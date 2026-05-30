058c2705bdae499ff6e273eeac8fbd493f8356b7
Gabriel <gabrielobandogarcia100@gmail.com>
2026-05-29 23:56:05 -0600

    CON-79 feat: implementar clase TiposDocumentoFiscal

--- src/main/java/esfe/dominio/TipoDocumentoFiscal.java
+++ src/main/java/esfe/dominio/TipoDocumentoFiscal.java
@@ -1,5 +1,41 @@
 package esfe.dominio;
 
 public class TipoDocumentoFiscal {
+    private int tipoDocumentoFiscalId;
+    private String codigo;
+    private String nombre;
 
+    public TipoDocumentoFiscal() {
+
+    }
+
+    public TipoDocumentoFiscal(int tipoDocumentoFiscalId, String codigo, String nombre) {
+        this.tipoDocumentoFiscalId = tipoDocumentoFiscalId;
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public int getTipoDocumentoFiscalId() {
+        return tipoDocumentoFiscalId;
+    }
+
+    public void setTipoDocumentoFiscalId(int tipoDocumentoFiscalId) {
+        this.tipoDocumentoFiscalId = tipoDocumentoFiscalId;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
