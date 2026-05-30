6ef73af730f640133f953c32874ebdc9dfb933d3
Gabriel Obando <gabrielobandogarcia100@gmail.com>
2026-05-30 10:13:26 -0600

    Implementación de la entidad TipoCuenta: dominio, persistencia y pruebas - Oscar (#4)
    * feat: agregar la clase TipoCuenta Realiazado por : Oscar Ramirez
    
    * feat: agregar la clase de prueba TipoCuenta Realiazado por : Oscar Ramirez
    
    * CON-13  feat: agregar la clase TipoCuenta Realiazado por : Oscar Ramirez
    
    * CON-16 feat: agregar la clase TipoCuentaTest Realiazado por : Oscar Ramirez
    
    ---------
    
    Co-authored-by: Oscar Ramirez <barleyunmain@gmail.com>

--- src/main/java/esfe/dominio/TipoCuenta.java
+++ src/main/java/esfe/dominio/TipoCuenta.java
@@ -1,4 +1,52 @@
 package esfe.dominio;
 
+
 public class TipoCuenta {
+
+    private int tipoCuentaId;
+    private String nombreTipo;
+    private char naturaleza;
+
+    public TipoCuenta() {
+    }
+
+    public TipoCuenta(int tipoCuentaId, String nombreTipo, char naturaleza) {
+        this.tipoCuentaId = tipoCuentaId;
+        this.nombreTipo = nombreTipo;
+        this.naturaleza = naturaleza;
+    }
+
+    public int getTipoCuentaId() {
+        return tipoCuentaId;
+    }
+
+    public void setTipoCuentaId(int tipoCuentaId) {
+        this.tipoCuentaId = tipoCuentaId;
+    }
+
+    public String getNombreTipo() {
+        return nombreTipo;
+    }
+
+    public void setNombreTipo(String nombreTipo) {
+        this.nombreTipo = nombreTipo;
+    }
+
+    public char getNaturaleza() {
+        return naturaleza;
+    }
+
+    public void setNaturaleza(char naturaleza) {
+        this.naturaleza = naturaleza;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoCuenta{" +
+                "tipoCuentaId=" + tipoCuentaId +
+                ", nombreTipo='" + nombreTipo + '\'' +
+                ", naturaleza=" + naturaleza +
+                '}';
+    }
 }
+
