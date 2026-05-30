6134af93f8a0c96f4de5bd0182ba7ddf3197b14e
Andrea <denissedepaz16@gmail.com>
2026-05-29 20:03:34 -0600

    feat: crear entidad Rol para la capa de dominio-Finalizado - Andrea Denisse Ortiz De Paz

--- src/main/java/esfe/dominio/Rol.java
+++ src/main/java/esfe/dominio/Rol.java
@@ -1,4 +1,55 @@
 package esfe.dominio;
 
 public class Rol {
-}
+
+        private int idRol;
+        private String nombreRol;
+        private String descripcion;
+
+
+        public Rol() {
+        }
+
+
+        public Rol(String nombreRol, String descripcion) {
+            this.nombreRol = nombreRol;
+            this.descripcion = descripcion;
+        }
+
+
+        public Rol(int idRol, String nombreRol, String descripcion) {
+            this.idRol = idRol;
+            this.nombreRol = nombreRol;
+            this.descripcion = descripcion;
+        }
+
+
+        public int getIdRol() {
+            return idRol;
+        }
+
+        public void setIdRol(int idRol) {
+            this.idRol = idRol;
+        }
+
+        public String getNombreRol() {
+            return nombreRol;
+        }
+
+        public void setNombreRol(String nombreRol) {
+            this.nombreRol = nombreRol;
+        }
+
+        public String getDescripcion() {
+            return descripcion;
+        }
+
+        public void setDescripcion(String descripcion) {
+            this.descripcion = descripcion;
+        }
+
+        @Override
+        public String toString() {
+            return "Rol{" + "idRol=" + idRol + ", nombreRol=" + nombreRol + ", descripcion=" + descripcion + '}';
+        }
+    }
