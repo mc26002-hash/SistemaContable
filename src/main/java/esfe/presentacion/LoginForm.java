60ed2bcfd74422bad5512d5145be7481bf61b3e7
Oscar Ramirez <barleyunmain@gmail.com>
2026-06-11 17:55:14 -0600

    fix: corregir vinculacion de componentes y error de contentPane null en TipoCuentaForm

--- src/main/java/esfe/presentacion/LoginForm.java
+++ src/main/java/esfe/presentacion/LoginForm.java
@@ -4,4 +4,9 @@ import javax.swing.*;
 
 public class LoginForm extends JDialog {
 
+    private JPanel mainPanel;
+    private JButton deshabilitarButton;
+    private JButton editarButton;
+    private JButton nuevaCuentaButton;
+    private JTable table1;
 }
